316022c29251aea4ff858fa60766e995298c4319
Danpr1984 <daniel.porras.rosses@gmail.com>
2023-10-13 10:18:34 +0000

    Added class to views.py, paths to url, created models and created ColorList.js file

--- color_music_wheel/music_app/models.py
+++ color_music_wheel/music_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Color(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    colors = models.ManyToManyField(Color)
+from django.contrib.auth.models import User
+
+class UserColorMusic(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+    music = models.ManyToManyField(Song)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.color.name}'
--- color_music_wheel/music_app/views.py
+++ color_music_wheel/music_app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from .models import Color
+from .serializers import ColorSerializer
 
-# Create your views here.
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def color_list(request):
+    colors = Color.objects.filter(user=request.user)
+    serializer = ColorSerializer(colors, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
