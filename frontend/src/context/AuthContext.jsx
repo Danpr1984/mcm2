e961d52cafae00346e9c36af4852ee655b992487
Daniel Porras <dapr247@gmail.com>
2024-02-09 08:29:07 +0100

    added response to debug

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -58,6 +58,9 @@ export default function AuthContextProvider({ children }) {
         credentials: "include",
       });
 
+      response.text().then(console.log); // Temporarily log the response text
+
+
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
