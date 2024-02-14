221e90a213d24036c6e86f5ada4dda1606746f2f
Daniel Porras <dapr247@gmail.com>
2024-02-14 13:43:46 +0100

    Update database settings for Heroku

--- backend/color_wheel/settings.py
+++ backend/color_wheel/settings.py
@@ -86,9 +86,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'color_wheel.wsgi.application'
 
 # Database
-DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'))
-}
+if 'DEVELOPMENT' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }}
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        }
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
