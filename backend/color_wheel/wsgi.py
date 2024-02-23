21ac84c0d24188cc896d0aeeaa62eab7db111d37
DarrachBarneveld <130994206+DarrachBarneveld@users.noreply.github.com>
2024-02-23 17:32:38 +0000

    Removed backend module

--- backend/color_wheel/wsgi.py
+++ backend/color_wheel/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.color_wheel.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'color_wheel.settings')
 
 application = get_wsgi_application()
