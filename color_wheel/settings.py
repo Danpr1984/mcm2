b09c542483ae3cbe30c9f6195edd8846c1257e03
Daniel Porras <dapr247@gmail.com>
2024-05-28 16:26:41 +0200

    Added URL

--- color_wheel/settings.py
+++ color_wheel/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
 
 ALLOWED_HOSTS = ['http://127.0.0.1:8000',
                 "http://localhost:3000", 
-                "https://music-colorwheel-097faf076de0.herokuapp.com",
+                "music-colorwheel-097faf076de0.herokuapp.com",
                 'localhost']
 
 CORS_ALLOW_ALL_ORIGINS = True
