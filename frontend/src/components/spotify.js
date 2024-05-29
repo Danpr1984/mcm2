7127f4d4d9aa7540e865fc7f7be228f6fc8680c4
Daniel Porras <dapr247@gmail.com>
2024-05-29 08:51:31 +0200

    Adjusted URL

--- frontend/src/components/spotify.js
+++ frontend/src/components/spotify.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const authEndpoint = "https://accounts.spotify.com/authorize?";
 const clientId = "660bc9f960ec49bf9d1a5e944c24dfbd";
-const redirectUri = "https://music-colorwheel-097faf076de0.herokuapp.com/";
+const redirectUri = "https://music-colorwheel-097faf076de0.herokuapp.com";
 // const redirectUri = "http://localhost:8000/";
 
 const scopes = [
